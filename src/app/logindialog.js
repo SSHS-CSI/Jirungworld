c358551d31e2a10fb74b3e80498fbf6a481b5e77
ulysseyson <ulysseyson57@gmail.com>
2019-12-27 14:05:10 +0900

    Logindialog update

--- src/app/logindialog.js
+++ src/app/logindialog.js
@@ -4,8 +4,7 @@ const Table = require("@material-ui/core/Table").default;
 const TableBody = require("@material-ui/core/TableBody").default;
 const TableCell = require("@material-ui/core/TableCell").default;
 const TableRow = require("@material-ui/core/TableRow").default;
-const TextField = require
- '@material-ui/core/TextField';
+const TextField = require("@material-ui/core/TextField").default;
 
 const ClosableDialog = require("./closable-dialog.js");
 
@@ -21,11 +20,11 @@ module.exports = ({
                 <TableRow>
                     <TableCell component="th" scope="row" variant="head">아이디
                     </TableCell>
-                    <TableCell scope="row">:</TableCell>
+                    <TextField id="standard-basic" label="Standard" />
                 </TableRow>
                 <TableRow>
                     <TableCell component="th" scope="row" variant="head">비밀번호</TableCell>
-                    <TableCell scope="row">:</TableCell>
+                    <TextField id="standard-basic" label="Standard" />
                 </TableRow>
             </TableBody>
         </Table>
